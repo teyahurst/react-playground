74dc35bfa875a3380f97b797daeb69fcdccf12aa
Teya Hurst <67305351+teyahurst@users.noreply.github.com>
2021-04-17 14:53:39 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
-import HelloWorld from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
+import Accordian from './state-drills/Accordian'
 
 
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 
 
@@ -12,9 +25,7 @@ import RouletteGun from './state-drills/RouletteGun';
 function App() {
   return (
     <main className='App'>
-      <RouletteGun/>
-      <HelloWorld />
-      <Bomb />
+      <Accordian sections={sections}/>
     </main>
   )
 }
